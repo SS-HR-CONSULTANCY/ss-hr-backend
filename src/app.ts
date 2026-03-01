f94fb9b04e7edd3eb0ed12ac510337bee3f9d93c
Full Stack Solutions <fullstacksolutions101@gmail.com>
2026-03-01 14:22:46 +0530

    Trigger deployment

--- src/app.ts
+++ src/app.ts
@@ -72,7 +72,7 @@ if (appConfig.nodeEnv === 'development') {
     },
   }));
 }
-// test
+// Triggering backend CI/CD pipeline
 app.use('/api/s3', S3Router);
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
